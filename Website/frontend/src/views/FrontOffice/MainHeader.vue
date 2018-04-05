2b04c4a124c6c5f54210fc1089995a388ca40ae0
maissenayed <ayed.maissen@gmail.com>
2018-04-06 00:04:47 +0100

    first home

---
+++ Website/frontend/src/views/FrontOffice/MainHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="panel-heading">
+      <nav>
+        <ul class="list-inline">
+          <li>
+            <router-link :to="{ name: 'home' }">Home</router-link>
+          </li>
+          <li v-if="!$auth.check()" class="pull-right">
+            <router-link :to="{ name: 'login' }">Login</router-link>
+          </li>
+          <li v-if="!$auth.check()" class="pull-right">
+            <router-link :to="{ name: 'register' }">Register</router-link>
+          </li>
+          <li v-if="$auth.check()" class="pull-right">
+            <a href="#" @click.prevent="$auth.logout()">Logout</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Website/frontend/src/views/FrontOffice/header.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <v-toolbar fixed class="white" id="">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+       <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat large color="primary" v-if="!$auth.check()" to="/" active-class> Home</v-btn>
+        <v-btn flat large color="primary" v-if="!$auth.check()" to= "/session/login" >Login</v-btn>
+        <v-btn flat large color="primary" to= "/session/sign-up" >Register</v-btn>
+        <v-btn flat large color="primary" v-if="$auth.check()" class="pull-right"><a href="#" @click.prevent="$auth.logout()">Logout</a></v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+</template>
+
+<script>
+
+
+export default {
+
+	    data () {
+      return {
+        title: 'Your Logo'
+      }
+}}
+
+</script>
+
+
+<style scoped>
+#logo{
+	position: fixed;
+}
+
+#cardbox{
+	display: inline;
+}
+
+
+</style>
